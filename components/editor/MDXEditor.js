f623aaec610c2f664ec77fe313a35fd4225a523b
Anthony Kuang <kuanganthony@gmail.com>
2021-10-24 05:05:23 -0700

    forced push for editor

--- components/editor/MDXEditor.js
+++ components/editor/MDXEditor.js
@@ -1,4 +1,4 @@
-import React, { useMemo, memo, useCallback } from 'react';
+import { useMemo, memo, useCallback } from 'react';
 import { VFileMessage } from 'vfile-message';
 import CodeMirror from 'rodemirror';
 import { basicSetup } from '@codemirror/basic-setup';
